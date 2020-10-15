ca4bbda6d660ee57775fcd1a8a5eb264d1424e79
DANIPC\Daniela <dgleivaencina@gmail.com>
2020-10-15 19:53:57 -0300

    controlador de producto y vista :

---
+++ src/main/java/com/web/controladores/ProductoController.java
@@ -0,0 +1,13 @@
+package com.web.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductoController {
+
+	@GetMapping("/producto")
+	public String producto(){
+		return "producto";
+	}
+}
