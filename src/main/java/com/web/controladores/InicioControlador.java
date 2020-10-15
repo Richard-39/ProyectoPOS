d83050e1f5422e77e324ab1e95915d34ae1fbbef
richard-39 <ricardo.abarza.v@zoho.com>
2020-10-15 19:12:47 -0300

    Proyecto inicial con controlador y vista html

---
+++ src/main/java/com/web/controladores/InicioControlador.java
@@ -0,0 +1,14 @@
+package com.web.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioControlador {
+
+	@GetMapping({"/", "inicio"})
+	public String inicio() {
+		return "inicio";
+	}
+	
+}
