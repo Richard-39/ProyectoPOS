781c6b38b0b150a889ad6856453284c13e092268
DANIPC\Daniela <dgleivaencina@gmail.com>
2020-10-23 16:00:06 -0300

    cambios en el controlador

--- src/main/java/com/web/controladores/ProductoController.java
+++ src/main/java/com/web/controladores/ProductoController.java
@@ -1,13 +1,22 @@
 package com.web.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.web.Servicio.ProductoServicio;
 
 @Controller
 public class ProductoController {
 
-	@GetMapping("/producto")
-	public String producto(){
-		return "producto";
+	@Autowired
+	private ProductoServicio productoDao;
+	
+	@GetMapping("/listarProductos")
+	public String producto(Model model){
+		model.addAttribute("titulo", "Listado de productos");
+		model.addAttribute("productos", productoDao.getAllProductos());
+		return "productos";
 	}
 }
