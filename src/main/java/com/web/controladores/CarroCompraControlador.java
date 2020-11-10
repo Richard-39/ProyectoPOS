4b8f9c13fcef619d47a43eee5cd4212c7896e488
richard-39 <ricardo.abarza.v@zoho.com>
2020-11-10 18:51:22 -0300

    Corrección bug de retroceder y se hace doble venta

--- src/main/java/com/web/controladores/CarroCompraControlador.java
+++ src/main/java/com/web/controladores/CarroCompraControlador.java
@@ -7,11 +7,15 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.web.modelo.Boleta;
 import com.web.modelo.ItemBoleta;
@@ -19,6 +23,7 @@ import com.web.modelo.Producto;
 import com.web.servicio.IBoleta;
 import com.web.servicio.ICarroCompra;
 import com.web.servicio.IProducto;
+import com.web.vo.BoletaVO;
 import com.web.vo.ProductoVO;
 
 @Controller
@@ -61,6 +66,11 @@ public class CarroCompraControlador {
 		Boleta boleta = new Boleta();
 		boleta.setItemBoleta(carroCompraServicio.obtenerItems());
 		
+		if (boleta.getItemBoleta().size() <= 0) {
+			model.addAttribute("mensaje", "El carro de compra está vacio.");
+			return "forward:/listarProductos";
+		}
+		
 		model.addAttribute("boleta", boleta);
 		model.addAttribute("total", Boleta.calcularMonto(carroCompraServicio.obtenerItems()));
 		
@@ -74,13 +84,22 @@ public class CarroCompraControlador {
 	}
 	
 	@PostMapping("/resumenPago")
-	public String saveBooks(@ModelAttribute Boleta boleta, Model model, @RequestParam Integer montoPago) {
-		for (ItemBoleta iterable_element : boleta.getItemBoleta()) {
-			System.out.println(iterable_element);
-		}
+	public ModelAndView resumenPago(@ModelAttribute Boleta boleta, RedirectAttributes model, @RequestParam(required = false, defaultValue = "0") Integer montoPago) {
+//		for (ItemBoleta iterable_element : boleta.getItemBoleta()) {
+//			System.out.println(iterable_element);
+//		}
+//		
+//		System.out.println("monto pago: " + montoPago);
+//		System.out.println("forma pago: " + boleta.getFormaPago());
 		
-		System.out.println("monto pago: " + montoPago);
-		System.out.println("forma pago: " + boleta.getFormaPago());
+		
+		// Asigna a cada item boleta el producto correspondiente
+		
+		
+		if(boleta.getItemBoleta().size() <= 0 || carroCompraServicio.obtenerItems().size() <=0) {
+			model.addAttribute("mensaje", "El carro de compra está vacio.");
+			return new ModelAndView ("redirect:/listarProductos");
+		}
 		
 		for (ItemBoleta item : boleta.getItemBoleta()) {
 			item.setProducto(productoServicio.findById(item.getProducto().getIdProducto()).getProductos().get(0));
@@ -88,13 +107,40 @@ public class CarroCompraControlador {
 		
 		boleta.setFecha(new Date());
 		boleta.setMonto(Boleta.calcularMonto(boleta.getItemBoleta()));
-		boletaServicio.save(boleta);
-		model.addAttribute("boleta", boleta);
-		model.addAttribute("montoPago", montoPago);
-		model.addAttribute("vuelto", montoPago - boleta.getMonto());
 		
+		if(boleta.getFormaPago().equals("efectivo")) {
+			
+			if(montoPago - boleta.getMonto() < 0) {
+				model.addAttribute("mensaje", "Monto Pago no puede ser menor que el total de la Boleta.");
+				return new ModelAndView ("redirect:/carroCompra/");
+			}
+			model.addFlashAttribute("montoPago", montoPago);	
+			model.addFlashAttribute("vuelto", montoPago - boleta.getMonto());
+			
+		} else {
+			model.addFlashAttribute("montoPago", 0);
+			model.addFlashAttribute("vuelto", 0);
+		}
+		
+		model.addFlashAttribute("boleta", boleta);
+		boletaServicio.save(boleta);	
 		carroCompraServicio.limpiarCarro();
 		
-	    return "resumenPago";
+		System.out.println("Boleta vendida !");
+		
+		ModelAndView mav = new ModelAndView ("redirect:/resumenPagoView");
+		
+	    return new ModelAndView ("redirect:/carroCompra/resumenPagoView");
+	}
+	
+	@GetMapping("/resumenPagoView")
+	public ModelAndView resumenPagoView(@ModelAttribute("boleta") Boleta boleta, ModelMap model ) {
+		
+		if(boleta.getItemBoleta().size() <= 0) {
+			return new ModelAndView ("redirect:/listarProductos");
+		}
+		
+		model.addAttribute("boleta", boleta);
+		return new ModelAndView("resumenPago", model);
 	}
 }
