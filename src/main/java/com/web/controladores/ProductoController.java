a267e21b3937cbf8f0988bb71c793b8233bb93bd
DANIPC\Daniela <dgleivaencina@gmail.com>
2020-11-13 16:08:39 -0300

    mejoras en el html producto y carro de compras

--- src/main/java/com/web/controladores/ProductoController.java
+++ src/main/java/com/web/controladores/ProductoController.java
@@ -4,17 +4,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
+import com.web.modelo.Producto;
 import com.web.servicio.IProducto;
 import com.web.vo.ProductoVO;
 
 @Controller
+@RequestMapping("/productos")
 public class ProductoController {
 
 	@Autowired
 	private IProducto productoDao;
 	
-	@GetMapping("/listarProductos")
+	@GetMapping("/crear")
+	public ModelAndView crearProducto (Model model) {
+		ModelAndView modelAndView = new ModelAndView ("crearProducto");
+		modelAndView.addObject("producto", new Producto());
+		return modelAndView;
+	}
+	
+	@PostMapping("/crear")
+	public RedirectView crearProducto (@ModelAttribute Producto producto) {
+		productoDao.save(producto);
+		return new RedirectView("/productos/listar");
+		
+	}
+	@GetMapping("/administrar")
+	public String admiProducto(Model model){
+		ProductoVO productoVo = productoDao.findAll();
+		model.addAttribute("productos", productoVo.getProductos());
+		model.addAttribute("mensaje", productoVo.getMensaje());
+		model.addAttribute("codigo", productoVo.getCodigo());
+		return "administrarProductos";
+	}
+	
+	
+	@GetMapping("/listar")
 	public String producto(Model model){
 		model.addAttribute("titulo", "Listado de productos");
 		ProductoVO productoVo = productoDao.findAll();
